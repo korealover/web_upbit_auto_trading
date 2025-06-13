9395efadd56861c10df59e35e2dda51c1ab4c907
Sun Jun Ho <2jsystems@gmail.com>
2025-06-13 10:08:22 +0900

    run.py 수정

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
-
 from app import create_app, socketio
+import eventlet
 
+eventlet.monkey_patch()
 app = create_app()
 
 if __name__ == '__main__':
-    # WebSocket을 지원하는 서버 실행
+    # WebSocket 서버 설정
     socketio.run(app,
                 debug=True,
                 host='0.0.0.0',
                 port=5000,
-                allow_unsafe_werkzeug=True)
+                allow_unsafe_werkzeug=True,
+                ping_timeout=60,
+                ping_interval=25,
+                cors_allowed_origins="*")
