4ea078e5c8a14577345eeab2e0f9b1fd66e0eb1b
jhsun <jhsun@sigongeducation.co.kr>
2025-06-13 10:38:22 +0900

    메노리 누수 방지, 성능 최적화

--- run.py
+++ run.py
@@ -6,11 +6,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # WebSocket 서버 설정
-    socketio.run(app,
-                debug=True,
-                host='0.0.0.0',
-                port=5000,
-                allow_unsafe_werkzeug=True,
-                ping_timeout=60,
-                ping_interval=25,
-                cors_allowed_origins="*")
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True, ping_timeout=60, ping_interval=25, cors_allowed_origins="*")
