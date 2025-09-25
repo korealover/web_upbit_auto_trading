5a64daa695fcbc73dec4345aa7e2740e7df3d0e7
jhsun <jhsun@sigongeducation.co.kr>
2025-09-25 09:35:34 +0900

    매수금액 및 주문가능금액 증액

--- app/forms.py
+++ app/forms.py
@@ -79,13 +79,13 @@ class TradingSettingsForm(FlaskForm):
 
     # 공통 설정
     # 기존 SelectField를 IntegerField와 FloatField로 변경
-    buy_amount = IntegerField('매수 금액 (원)', validators=[NumberRange(min=5000, max=50000)], default=12000)
+    buy_amount = IntegerField('매수 금액 (원)', validators=[NumberRange(min=5000, max=5000000)], default=12000)
     min_cash = IntegerField('최소 보유 현금량', validators=[NumberRange(min=0, max=2000000)], default=50000)
     sleep_time = IntegerField('거래 간격 (초)', validators=[NumberRange(min=10, max=600)], default=60)
     sell_portion = FloatField('매도 비율', validators=[NumberRange(min=0.1, max=1.0)], default=0.5)
 
     # 새로 추가: 주문 가능 금액 필드
-    max_order_amount = IntegerField('주문 가능 금액 (원)', validators=[NumberRange(min=0, max=1000000)], default=50000)
+    max_order_amount = IntegerField('주문 가능 금액 (원)', validators=[NumberRange(min=0, max=200000000)], default=50000)
 
     prevent_loss_sale = SelectField('손절 금지', choices=[('Y', '예'), ('N', '아니오')], default='Y')
     long_term_investment = SelectField('장기 투자', choices=[('Y', '예'), ('N', '아니오')], default='N')
